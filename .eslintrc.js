5c5dc37091a9717823c6c381c95ab39d3fff12dc
feng <719542112@qq.com>
2018-09-27 16:23:32 +0200

    aa

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': 'off'
+    'no-console': 'off',
+    'semi':0
   }
 };
